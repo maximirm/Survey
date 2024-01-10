7873005e5de30c8fecd5f62e4ad69525706e07ea
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-10 15:17:17 +0100

    add unittests

--- tests/services/test_survey_service.py
+++ tests/services/test_survey_service.py
@@ -1,11 +1,10 @@
 import unittest
 from unittest.mock import patch, MagicMock
+from uuid import uuid4
 
 from app.services.exceptions.survey_not_found_exception import SurveyNotFoundException
-
 from app.services.survey_service import get_survey, get_surveys_by_creator_id, create_survey, delete_survey, \
     delete_surveys_by_creator_id
-from uuid import uuid4
 
 
 class TestSurveyService(unittest.TestCase):
@@ -63,3 +62,41 @@ class TestSurveyService(unittest.TestCase):
 
         self.assertEqual(mock_survey, result)
         mock_create_survey.assert_called_once_with(self.db, mock_survey)
+
+    @patch("app.database.crud.survey_crud.delete_survey")
+    def test_delete_survey(self, mock_delete_survey):
+        survey_id = uuid4()
+        mock_survey = MagicMock()
+        mock_delete_survey.return_value = mock_survey
+
+        result = delete_survey(self.db, survey_id)
+
+        self.assertEqual(result, {"message": f"Survey with ID {str(survey_id)} deleted successfully"})
+        mock_delete_survey.assert_called_once_with(self.db, survey_id)
+
+    @patch("app.database.crud.survey_crud.delete_survey", return_value=None)
+    def test_delete_survey_not_found(self, mock_delete_survey):
+        survey_id = uuid4()
+
+        with self.assertRaises(SurveyNotFoundException):
+            delete_survey(self.db, survey_id)
+        mock_delete_survey.assert_called_once_with(self.db, survey_id)
+
+    @patch("app.database.crud.survey_crud.delete_surveys_by_creator_id")
+    def test_delete_surveys_by_creator_id(self, mock_delete_surveys_by_creator_id):
+        creator_id = uuid4()
+        mock_surveys = MagicMock()
+        mock_delete_surveys_by_creator_id.return_value = mock_surveys
+
+        result = delete_surveys_by_creator_id(self.db, creator_id)
+
+        self.assertEqual(result, {"message": f"Surveys for creator-ID {str(creator_id)} deleted successfully"})
+        mock_delete_surveys_by_creator_id.assert_called_once_with(self.db, creator_id)
+
+    @patch("app.database.crud.survey_crud.delete_surveys_by_creator_id", return_value=None)
+    def test_delete_surveys_by_creator_id_not_found(self, mock_delete_surveys_by_creator_id):
+        creator_id = uuid4()
+
+        with self.assertRaises(SurveyNotFoundException):
+            delete_surveys_by_creator_id(self.db, creator_id)
+        mock_delete_surveys_by_creator_id.assert_called_once_with(self.db, creator_id)
