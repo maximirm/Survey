454ba36e3a648a9e02c9b3b9fc2b1cb611a023fe
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-11 15:45:26 +0100

    add model to schema conversion

--- app/services/question_service.py
+++ app/services/question_service.py
@@ -8,6 +8,8 @@ from app.database.schemas import schemas
 from app.database.crud import question_crud
 from sqlalchemy.orm import Session
 
+from app.services.utils.converter import convert_question_model_to_schema
+
 
 def get_question(db: Session, question_id: UUID):
     db_question = question_crud.get_question(db, question_id)
@@ -15,7 +17,7 @@ def get_question(db: Session, question_id: UUID):
         raise QuestionNotFoundException(
             f"Question with ID {str(question_id)} not found"
         )
-    return db_question
+    return convert_question_model_to_schema(db_question)
 
 
 def get_questions_by_survey_id(db: Session, survey_id: UUID):
@@ -24,12 +26,12 @@ def get_questions_by_survey_id(db: Session, survey_id: UUID):
         raise QuestionNotFoundException(
             f"No Questions for Survey-ID {str(survey_id)} found"
         )
-    return db_questions
+    return [convert_question_model_to_schema(db_question) for db_question in db_questions]
 
 
 def create_question(db: Session, question: schemas.QuestionCreate):
     try:
-        return question_crud.create_question(db, question)
+        return convert_question_model_to_schema(question_crud.create_question(db, question))
     except IntegrityError:
         raise ForeignKeyNotFoundException(
             f"Can not create Question. No Survey with ID {str(question.survey_id)} found"
--- app/services/response_service.py
+++ app/services/response_service.py
@@ -7,24 +7,26 @@ from app.database.schemas import schemas
 from app.database.crud import response_crud
 from sqlalchemy.exc import IntegrityError
 
+from app.services.utils.converter import convert_response_model_to_schema
+
 
 def get_response(db: Session, response_id: UUID):
     db_response = response_crud.get_response(db, response_id)
     if db_response is None:
         raise ResponseNotFoundException(f"Response with ID {str(response_id)} not found")
-    return db_response
+    return convert_response_model_to_schema(db_response)
 
 
 def get_responses_by_question_id(db: Session, question_id: UUID):
     db_responses = response_crud.get_responses_by_question_id(db, question_id)
     if not db_responses:
         raise ResponseNotFoundException(f"No Responses for Question-ID {str(question_id)} found")
-    return db_responses
+    return [convert_response_model_to_schema(db_response) for db_response in db_responses]
 
 
 def create_response(db: Session, response: schemas.ResponseCreate):
     try:
-        return response_crud.create_response(db, response)
+        return convert_response_model_to_schema(response_crud.create_response(db, response))
     except IntegrityError:
         raise ForeignKeyNotFoundException(
             f"Can not create Response. No Question with ID {str(response.question_id)} found"
